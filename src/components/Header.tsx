e3f82d3e46a6789777e7635897151be26b29b725
Juya <juyaamirm@gmail.com>
2024-10-07 09:29:34 +0200

    deploy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,8 +14,9 @@ const Header = () => {
   }
 
   return (
-    <header className='min-h-16'>
-      <div className='flex flex-wrap justify-between items-center gap-2 p-4'>
+    <header className='min-h-16 '>
+      <div className='desktop-nav '>
+        <div className='flex flex-wrap justify-between items-center gap-2 p-4'>
         <div className='flex justify-center items-center'>
           <Link to='/'><img src={logo} alt='logo' className='h-14' /></Link>
           <p className='font-bold text-green-600 text-lg'>Wellness <span className='text-cyan-600'>Hub</span></p>
@@ -28,21 +29,23 @@ const Header = () => {
         <div className='hidden md:flex'>
           <button className='bg-green-600 hover:bg-green-800 text-white px-2 py-1 rounded-md'>Sign In</button>
         </div>
-      </div>
-      <nav className='hidden md:flex flex-wrap justify-center gap-8 items-center bg-slate-100 text-green-600 border-y border-slate-400 desktop-nav'>
+        </div>
+      <nav className=' flex  justify-center gap-8 items-center bg-slate-100 text-green-600 border-y border-slate-400 '>
         {navbar.map((item) => (
           <Link key={item.path} to={item.path} className='p-2 hover:bg-green-800 hover:text-white md:text-xl lg:text-2xl'>
             {item.title}
           </Link>
         ))}
       </nav>
+      </div>
+      
       {/* Mobile Navbar */}
-      <nav className='md:hidden fixed top-0 left-0 right-0 z-50 bg-white shadow-lg'>
+      <nav className='md:hidden  top-0 left-0 right-0 z-50 bg-white shadow-l'>
       
           <Link to='/' className='flex items-center justify-center pt-2'><img src={logo} alt='logo' className='h-14 ' />
           <p className='font-bold text-green-600 text-lg'>Wellness <span className='text-cyan-600'>Hub</span></p></Link>
         
-        <div className='flex  justify-between'>
+        <div className='flex  justify-between  '>
             <button className='px-4 py-2 rounded-md  text-2xl' onClick={handleToggle}>
               {showNav ? <span>&#10005;</span> : <span>&#9776;</span>}
             </button>
