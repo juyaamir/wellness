218a2dbfa89bcd6312fe95003df3c8cae851757c
Juya <juyaamirm@gmail.com>
2024-10-07 08:45:32 +0200

    worked on serch part of mobile v

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,13 @@ import logo from '../assets/logo.jpg';
 
 const Header = () => {
   const [showNav, setShowNav] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
   const handleToggle = () => {
     setShowNav(!showNav);
   };
+  const handleSearchToggle = () => {
+    setShowSearch(!showSearch);
+  }
 
   return (
     <header className='min-h-16'>
@@ -34,20 +38,25 @@ const Header = () => {
       </nav>
       {/* Mobile Navbar */}
       <nav className='md:hidden fixed top-0 left-0 right-0 z-50 bg-white shadow-lg'>
-      <div className='flex justify-start items-center ml-2 mt-2'>
-          <Link to='/'><img src={logo} alt='logo' className='h-14' /></Link>
-          <p className='font-bold text-green-600 text-lg'>Wellness <span className='text-cyan-600'>Hub</span></p>
-        </div>
+      
+          <Link to='/' className='flex items-center justify-center pt-2'><img src={logo} alt='logo' className='h-14 ' />
+          <p className='font-bold text-green-600 text-lg'>Wellness <span className='text-cyan-600'>Hub</span></p></Link>
+        
         <div className='flex  justify-between'>
             <button className='px-4 py-2 rounded-md  text-2xl' onClick={handleToggle}>
               {showNav ? <span>&#10005;</span> : <span>&#9776;</span>}
             </button>
-            <div className='flex'>
-              <input type='search' placeholder='Search Wellness Hub...' className='' />
-              <button className=''>S</button>
-            </div>
+            <button className='focus:bg-gray-200 px-4 py-2' onClick={handleSearchToggle}>Search</button>
             <button className='px-4 py-2'>Sign In</button>
         </div>
+        {
+          showSearch && (
+            <div className='flex py-2 px-4 bg-gray-200'>
+              <input type="search" placeholder='Search Wellness Hub....' className='flex-grow rounded-tl-md rounded-bl-md p-2 outline-none border border-gray-400 shadow-md focus:border-green-400' />
+              <button className='bg-green-500 hover:bg-green-700 text-white font-bold p-2 rounded-tr-md rounded-br-md'>Go</button>
+            </div>
+          )
+        }
         {showNav && (
           <div className='flex flex-col items-center bg-slate-100 text-green-600 border-t border-slate-400 transition-all duration-300 ease-in-out'>
             {navbar.map((item) => (
